b25d3bc70844faca8d2a85dfb27d06758e170cdd
Krystian Gumulinski <k.gumulinski@gmail.com>
2024-06-12 20:39:53 +0200

    Added a dependency for sqlite3 database connection

--- bookmanagement/src/main/java/com/book/Main.java
+++ bookmanagement/src/main/java/com/book/Main.java
@@ -1,24 +1,7 @@
 package com.book;
 
-import com.book.interfaces.AuthorSearch;
-import com.book.interfaces.TitleSearch;
-
 public class Main {
     public static void main(String[] args) {
-        Library myLibrary = new Library();
-        Book book1 = new Book("Wiedzmin", "Wild Hunt", new Author("Andrzej", "Sapkowski"));
-        Book book2 = new Book("Wiedzmin", "Wild Hunt", new Author("Andrzej", "Sapkowski"));
-        System.out.println(book1.equals(book2));
-        myLibrary.addBookToShelf(book1);
-        myLibrary.addBookToShelf(book1);
-        myLibrary.addBookToShelf(book2);
-
-        AuthorSearch authorSearch = new AuthorSearch(new Author("Andrzej", "Lapkowski"));
-        TitleSearch titleSearch = new TitleSearch("Wiedzmin");
-
-        System.out.println(myLibrary.findBookByParameter(authorSearch));
-        myLibrary.printOutAllBooks();
-        // myLibrary.writeAllToFile();
-        myLibrary.writeSelectedBooksToFile(titleSearch);
+        System.out.println("Test");
     }
 }
